18a6dacb2f6a65acd49131e15fa9add5cbff0a88
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-04-18 00:43:56 -0300

    remove dir multimidia

--- src/app/multimidia/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function MultimidiaLayout({childrem}: MultimidiaLayout){
-    return(
-        <div>
-            <h1>layout multimidia</h1>
-            
-        </div>
-    )
-}
--- src/app/multimidia/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MultimidiaPage(){
-    return (
-        <div>
-            <h1>página</h1>
-            <h1>multimidia</h1>
-        </div>
-    )
-}
